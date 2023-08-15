ad2c0b42444a5cc36a3a281a2464144ae43643b5
lesponns <adamadakadam1903@gmail.com>
2023-08-15 20:48:12 +0300

    Add project files.

---
+++ SmartPro_34[15-08-23]/OverLoadMethod.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartPro_34_15_08_23_
+{
+    internal class OverLoadMethod
+    {
+        public void Topla(int n1, int n2)
+        {
+            int last = n1 + n2;
+            Console.Write("Toplama Methodu\n");
+            Console.WriteLine("Toplam: {0} + {1} = {2}\n" ,n1,n2,last);
+
+        }
+        public void Topla(int n1, int n2, int n3)
+        {
+            int last = n1 + n2 + n3;
+            Console.Write("Toplama Methodu\n");
+            Console.WriteLine("Toplam: {0} + {1} + {2} = {3}\n", n1, n2, n3, last);
+        }
+        public void Topla(double n1, double n2)
+        {
+            double last = n1 + n2;
+            Console.Write("Toplama Methodu\n");
+            Console.WriteLine("Toplam: {0} + {1} = {2}\n", n1, n2, last);
+        }
+        public void Topla(double n1, double n2, double n3)
+        {
+            double last = n1 + n2 + n3;
+            Console.Write("Toplama Methodu\n");
+            Console.WriteLine("Toplam: {0} + {1} + {2} = {3}\n", n1, n2, n3, last);
+        }
+        public void Topla()
+        {
+            Console.Write("Bir değer girmediniz.");        
+        }
+    }
+}
---
+++ SmartPro_34[15-08-23]/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartPro_34_15_08_23_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            OverLoadMethod yazdir = new OverLoadMethod();
+
+            yazdir.Topla(5,6);
+            yazdir.Topla(5,6,7);
+            yazdir.Topla(5.3,2.5);
+            yazdir.Topla(3.4,6.6,1.5);
+            yazdir.Topla();
+
+            Console.ReadKey();
+
+        }
+    }
+}
